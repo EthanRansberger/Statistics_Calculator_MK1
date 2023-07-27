fc7df70c15171157c85cfa50cd99e3bf6913a197
EthanRansberger <100398629+EthanRansberger@users.noreply.github.com>
2023-07-27 13:25:17 -0400

    t

--- zscore_calculator.py
+++ zscore_calculator.py
@@ -10,6 +10,29 @@ sd=None
 n_y=None
 locc=0
 
+qq = input("z,t,f,chi")
+if qq=='t'or qq=='f' or qq=='chi':
+    n=int(input("n:"))
+    if qq=='f':
+        n_y=int(input("n_y:"))
+pof = input("ppf (give probability) or cdf (return probability)")
+if pof == 'ppf':
+    p=float(input("probability:"))
+elif pof=='cdf':
+    
+    
+    if qq=='f':
+        
+        f=float(input("f value:"))
+    elif qq=='t':
+        t=float(input("t value"))
+    elif qq=='chi':
+        chi=float(input("chi value:"))
+    elif qq=='z':
+        z=float(input("z value:"))
+            
+        
+
 if n:
     df=n-1
 if n_y:
@@ -17,24 +40,31 @@ if n_y:
 
 if p:
     if sd:
-        pvalz=st.chisquare.pdf(p,df)
+       pvalz=st.chi2.ppf(p,df)
+       print("1-p \n", st.chi2.ppf((1-p),df) , "\n p")
     elif n:
         if n_y:
             pvalz=st.f.ppf(p,df,df_y) 
+            print("1-p \n", st.f.ppf((1-p),df,df_y) , "\n p")
         else:
             pvalz=st.t.ppf(p,df) 
+            print("1-p \n", st.t.ppf((1-p),df) , "\n p")
+          
     else:
         pvalz=st.norm.ppf(p)
-   
+        print("1-p \n", st.norm.ppf((1-p)) , "\n p")
 if z:
     pvalz=st.norm.cdf(z)
-    print(1-pvalz)
+    print("greater than probability \n", 1-pvalz, "\n less than probability")
 if t:
     pvalz=st.t.cdf(t,df)
+    print("greater than probability\n", 1-pvalz, "\n less than probability")
 if f:
     pvalz=st.f.cdf(f,df,df_y,loc=locc)
 if chisq:
-    pvalz=st.chisquare.cdf(chisq,df)
+    pvalz=st.chi2.cdf(chisq,df)
+    print("greater than probability \n", 1-pvalz, "\n less than probability")
+    
  
 
 print(pvalz)
